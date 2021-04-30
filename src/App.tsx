30ff09aa1d62302b7f6094d51f34a3089dd6fa24
João Dantas <joaostadantas@gmail.com>
2021-04-29 22:23:33 -0300

    innitial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
-import Modal from "react-modal";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
+
+
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -19,12 +23,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>      
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
