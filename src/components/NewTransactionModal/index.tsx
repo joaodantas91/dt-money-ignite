5364ec6db8f81fb44155bb28341f3fba63bf79ad
João Vitor Santa Brigida Dantas <joaodantas@iMac-de-Joao.local>
2021-05-18 21:50:33 -0300

    backup

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,8 +2,11 @@ import Modal from "react-modal";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
-import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import { FormEvent, useState } from "react";
+
+import { api } from "../../services/api";
+
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 Modal.setAppElement("#root");
 
@@ -24,12 +27,14 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       category,
       type,
-    });
+    };
+
+    api.post("/transactions", data);
   }
 
   return (
